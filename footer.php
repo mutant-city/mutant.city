a0fc31a54855d166ef10f97574503f2563f5c7dd
Nick T-bone Kiermaier <nickkiermaier@gmail.com>
2018-10-26 14:09:48 -0500

    upd

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <p><a href="#"><i class="fa fa-chevron-up"></i> Back to top</a></p>
     <div class="content has-text-centered">
-        This is a big ol' footer!
+        This is a big ol' footer! Put something here I guess.
         <?php wp_footer(); ?>
     </div>
 </footer>
